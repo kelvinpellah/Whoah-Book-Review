c42dc6462de07e722b90d7b36c12496777d2e86e
kelvin pella <kelvinpella@gmail.com>
2020-08-09 01:46:52 +0300

    Password Validation

--- frontend/gui/src/components/registration.js
+++ frontend/gui/src/components/registration.js
@@ -1,44 +1,75 @@
 import React from 'react';
 import {Link} from "react-router-dom";
 
+// initial state
 
-// Check password strength
-function PasswordStrength(props) {
-
-    let length_password = props.values.length;
-    let password_input = props.values;
-
-    if (password_input === ''){
-        return null
-    }
-    else {
-        return(
-        <p>Password is {length_password < 6 ? 'weak' : 'strong'}</p>
-        );
-    }
+const initialState = {
+    credentials: {
+        username:"",
+        password:"",    
+        email:"",    
+        confirmPassword:"",       
+    },
+    passwordError:"",    
+    confirmPasswordError:"",    
     
 }
 class RegistrationForm extends React.Component {
 
     constructor(props){
         super(props);
-        this.state = {
-            credentials: {
-                username:"",
-                password:"",
-                email:"",
-                confirm_password:"",
-            }
-        };
-        this.InputChange = this.InputChange.bind(this);
+        this.state = initialState;
+        
+        this.inputChange = this.inputChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
     
     // Change state to specific field in case of input changes
     
-    InputChange(event){
+    inputChange(event){
         const cred = this.state.credentials;
         cred[event.target.name]=event.target.value;
         this.setState({credentials: cred});
+        console.log(event.target.name);
+
+        
+    }
+
+    // Checking correctness of the form on submission
+
+    validateForm = () => {
+        let passwordError = "";
+        let confirmPasswordError ="";
+
+        // checking password for errors.
+
+        if (this.state.credentials.password.length < 6) {
+            passwordError = "Password is too short. A minimum of six characters required.";
+        }
+
+        if (this.state.credentials.password !== this.state.credentials.confirmPassword) {
+            confirmPasswordError = "Passwords don't match";
+        }
+
+        // Update state and Return the errors.
+        if (passwordError || confirmPasswordError) {
+            this.setState({passwordError, confirmPasswordError });
+            return false;
+        }
+
+        return true;
+
+    }
+    
+    // Form submission
+
+    handleSubmit = event => {
+        event.preventDefault();
+        const isValid = this.validateForm();
+        if (isValid) {
+            // Clear form after submission if correct
+            this.setState(initialState);
+        }
         
     }
      
@@ -46,21 +77,24 @@ class RegistrationForm extends React.Component {
     render() {
         return (
             <div>
-                <form>
+                <form onSubmit={this.handleSubmit}>
                     <h1>Welcome to Whoah!</h1>
                     <h2>Please create your account.</h2>
                     <label htmlFor="username">Username</label>
-                    <input type="text" id="username" name="username" value={this.state.credentials.username} onChange={this.InputChange}/><br />
+                    <input type="text" id="username" name="username" value={this.state.credentials.username} onChange={this.inputChange}/><br />
                     <label htmlFor="password">Password</label>
-                    <input type="password" id="password" name="password" value={this.state.credentials.password} onChange={this.InputChange}/><br />
+                    <input type="password" id="password" name="password" value={this.state.credentials.password} onChange={this.inputChange}/><br />
                     <div>
-                        <PasswordStrength values={this.state.credentials.password}/>
+                        {this.state.passwordError}
                     </div>
-                    <label htmlFor="confirm_password">Confirm Password</label>
-                    <input type="password" id="confirm_password" name="confirm_password" value={this.state.credentials.confirm_password} onChange={this.InputChange}/><br />
+                    <label htmlFor="confirmPassword">Confirm Password</label>
+                    <input type="password" id="confirmPassword" name="confirmPassword" value={this.state.credentials.confirmPassword} onChange={this.inputChange}/><br />
+                    <div>
+                        {this.state.confirmPasswordError}
+                    </div>
                     <label htmlFor="email">E-mail</label>
-                    <input type="email" id="email" name="email" value={this.state.credentials.email} onChange={this.InputChange}/><br />
-                    <button type="submit">Sign Up</button><br />
+                    <input type="email" id="email" name="email" value={this.state.credentials.email} onChange={this.inputChange}/><br />
+                    <button type="submit" >Sign Up</button><br />
                 </form>
                 <p>Already have an account?<Link to="/login">Sign in</Link></p>
             </div>
