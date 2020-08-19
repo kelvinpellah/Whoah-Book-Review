146e3f0ef7c221458405cc2002b696c6c15a5046
kelvin pella <kelvinpella@gmail.com>
2020-08-19 10:52:30 +0300

    changes

--- frontend/gui/src/containers/layout.js
+++ frontend/gui/src/containers/layout.js
@@ -1,21 +1,12 @@
 import React from 'react';
 import BaseRouter from "../routes"
-import { Layout } from 'antd';
 
-
-const { Header, Footer,Content } = Layout;
 
 class AppLayout extends React.Component {
     render() {
         return (
             <div>
-                <Layout>
-                    <Header>Header</Header>
-                    <Content>
-                        <BaseRouter />
-                    </Content>
-                    <Footer>Footer</Footer>
-                </Layout>
+                <BaseRouter />
             </div>)
     }
 }
