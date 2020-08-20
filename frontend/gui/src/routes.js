cebe4eddb05e61d02465388d660b46aeeea7a097
kelvin pella <kelvinpella@gmail.com>
2020-08-21 00:45:26 +0300

    Modifications for rendered components

--- frontend/gui/src/routes.js
+++ frontend/gui/src/routes.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import {Route, Switch} from "react-router-dom";
-import LoginForm from './components/login';
-import RegistrationForm from './components/registration';
 import LandingPage from "./components/landingpage";
 
 const BaseRouter = () => (
@@ -9,12 +7,6 @@ const BaseRouter = () => (
         <Switch>
             <Route exact path="/home">
                 <LandingPage/>
-            </Route>
-            <Route exact path="/login">
-                <LoginForm />
-            </Route>
-            <Route exact path="/register">
-                <RegistrationForm />
             </Route>
         </Switch>
     </div>
