5508f5d6b4352d571090f6dc38f42f1b10635714
kelvin pella <kelvinpella@gmail.com>
2020-08-27 18:09:55 +0300

    Modified live book search

--- frontend/gui/src/components/booksearch.js
+++ frontend/gui/src/components/booksearch.js
@@ -16,7 +16,7 @@ class BookSearch extends React.Component {
             message:'',
             loading: false
         }
-        
+        this.cancel= "";
         this.handleChange = this.handleChange.bind(this);
         this.fetchSearchResults=this.fetchSearchResults.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
@@ -24,20 +24,24 @@ class BookSearch extends React.Component {
 
     handleChange= (event) => {
         const query = event.target.value;
-        this.setState({query, message:'', loading:true}
+        this.setState({query, message:'', loading:true}, () => {
+            this.fetchSearchResults(query);
+        }
         );
 
     }
 
-    fetchSearchResults = async () => {
-
-        const query = this.state.query;
+    fetchSearchResults = async (query) => {
+        // Remove cancel token when we start
+        if(this.cancel){
+            this.cancel.cancel();
+        }
+        this.cancel = axios.CancelToken.source();
         try {
-                let res= await axios({
-                    method:'GET',
-                    url:`http://127.0.0.1:8000/api/book/?search=${query}`,
-                    headers: {'Content-Type': 'multipart/form-data'}
-                }).then(response => {
+                let res= await axios.get(
+                    `http://127.0.0.1:8000/api/book/?search=${query}`,
+                    { cancelToken: this.cancel.token}
+                ).then(response => {
                         console.log(response);
                     }).catch(error => {
                         console.log('The error is', error)
@@ -46,30 +50,26 @@ class BookSearch extends React.Component {
                 console.log(error.message)
             }
 
+        }   
+    
+        handleSubmit = event => {
+            event.preventDefault();
+            this.fetchSearchResults(this.state.query);
+            
         }
-        
-    handleSubmit = (event) => {
-        event.preventDefault();
-        this.fetchSearchResults();
-
-    }   
-  
 
     render() {
         return(
             <div>
                 <Form inline onSubmit={this.handleSubmit} >
-                    <InputGroup  >
+                    <Form.Group  >
                         <Form.Control 
                             type='input' 
                             value={this.state.query} 
                             onChange={this.handleChange} 
-                            placeholder="Search by title, author etc" />
-                        <InputGroup.Prepend>
-                            <InputGroup.Text id="basic-addon1">search</InputGroup.Text>
-                        </InputGroup.Prepend>    
-                    </InputGroup>
-                    <Button type='submit'>Submit</Button>
+                            placeholder="Search by title, author etc" />   
+                    </Form.Group>
+                    <Button className="ml-2" type='submit'>Submit</Button>
                 </Form>
             </div>
         )
