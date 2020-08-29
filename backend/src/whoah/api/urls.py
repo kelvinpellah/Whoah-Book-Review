90846bb998b45fe62983a09d9fce5f600f145a6a
kelvin pella <kelvinpella@gmail.com>
2020-08-29 22:46:28 +0300

    Serializer, viewset and url for book details

--- backend/src/whoah/api/urls.py
+++ backend/src/whoah/api/urls.py
@@ -1,6 +1,6 @@
 from rest_framework import routers
 from django.urls import include, path
-from .views import RegisterUserViewset, LoginViewset, BookViewset,BookSearchViewset
+from .views import RegisterUserViewset, LoginViewset, BookViewset,BookSearchViewset,BookDetailsViewset
 
 
 router = routers.DefaultRouter()
@@ -8,6 +8,7 @@ router.register('register', RegisterUserViewset)
 router.register('login', LoginViewset, basename='login-user')
 router.register('books', BookViewset)
 router.register('book', BookSearchViewset)
+router.register('book/:id', BookDetailsViewset)
 
 urlpatterns = router.urls
 
