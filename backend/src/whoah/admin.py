6891d26a54c850a6c560f82698113068be5063ad
kelvin pella <kelvinpella@gmail.com>
2020-09-03 00:26:23 +0300

    Created Comments Model

--- backend/src/whoah/admin.py
+++ backend/src/whoah/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Book
+from .models import Book, BookComment
 from import_export.admin import ImportExportModelAdmin
 
 @admin.register(Book)
+@admin.register(BookComment)
 class ViewAdmin(ImportExportModelAdmin):
     pass
